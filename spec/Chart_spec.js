a434d25125ffc4a49aa065396daae3c55e87bee7
Hugo-ter-Doest <hwl.ter.doest@gmail.com>
2018-11-07 00:11:57 +0100

    Refactored parser modules to classes

---
+++ spec/Chart_spec.js
@@ -0,0 +1,90 @@
+/*
+    Unit test for Chart.js using Jasmine
+    Copyright (C) 2018 Hugo W.L. ter Doest
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var typeOf = require('typeof');
+
+var Chart = require('../lib/natural/parsers/Chart.js');
+var Item = require('../lib/natural/parsers/CYKItem');
+
+
+describe('Chart', function() {
+  var chart;
+  var size = 6;
+  
+  it('should create an empty chart', function() {
+    chart = new Chart(size);
+    expect(chart.N).toEqual(size);
+    expect(chart.outgoing_edges.length).toEqual(size + 1);
+    expect(chart.incoming_edges.length).toEqual(size + 1);
+    expect(typeOf(chart.incoming_edges)).toEqual('array');
+    expect(typeOf(chart.outgoing_edges)).toEqual('array');
+  });
+
+
+  var i1 = new Item({
+    rule: {'lhs': 'S', 'rhs': ['NP', 'VP']},
+    from: 0,
+    to: 1
+  });
+  var i2 = new Item({
+    rule: {'lhs': 'S', 'rhs': ['NP', 'VP']},
+    from: 1,
+    to: 2
+  });
+  var i3 = new Item({
+    rule: {'lhs': 'S', 'rhs': ['NP', 'VP']},
+    from: 2,
+    to: 3
+  });
+  var i4 = new Item({
+    rule: {'lhs': 'S', 'rhs': ['NP', 'VP']},
+    from: 0,
+    to: 6
+  });
+  i4.setChildren([i1, i2, i3]);
+
+
+  it('should add items to the chart', function() {
+    chart.addItem(i1);
+    chart.addItem(i2);
+    chart.addItem(i3);
+    expect(chart.getItemsFromTo(0, 1)).toEqual([i1]);
+    expect(chart.getItemsFromTo(1, 2)).toEqual([i2]);
+    expect(chart.getItemsFromTo(2, 3)).toEqual([i3]);
+    expect(chart.getItemsFrom(0)).toEqual([i1]);
+    expect(chart.getItemsFrom(1)).toEqual([i2]);
+    expect(chart.getItemsFrom(2)).toEqual([i3]);
+    expect(chart.getItemsTo(1)).toEqual([i1]);
+    expect(chart.nrItemsTo(1)).toEqual(1);
+    expect(chart.getItemsTo(2)).toEqual([i2]);
+    expect(chart.nrItemsTo(2)).toEqual(1);
+    expect(chart.getItemsTo(3)).toEqual([i3]);
+    expect(chart.nrItemsTo(3)).toEqual(1);
+    expect(chart.getCompleteItemsFromTo(2, 3)).toEqual([i3]);
+    expect(chart.fullParseItems('S', 'cykitem')).toEqual([]);
+    chart.addItem(i4);
+    expect(chart.getCompleteItemsFromTo(0, 6)).toEqual([i4]);
+    expect(chart.fullParseItems('S', 'cykitem')).toEqual([i4]);
+    expect(chart.nrOfItems()).toEqual(4);
+  });
+
+
+  it('should generate parse trees from the chart', function() {
+    expect(chart.parseTrees('S', "cykitem")).toEqual(["S(S(NP,VP),S(NP,VP),S(NP,VP))"]);
+  });
+});
---
+++ spec/GrammarParser_spec.js
@@ -0,0 +1,233 @@
+/*
+    Unit test for GrammarParser.js using Jasmine
+    Copyright (C) 2016 Hugo W.L. ter Doest
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var fs = require('fs');
+var GrammarParser = require('../lib/natural/parsers/GrammarParser');
+
+var path = './spec/test_data/';
+var minimal_grammar_file = path + 'minimal_grammar.txt';
+var grammar_for_CFG_file = path + 'test_grammar_for_CFG.txt';
+var math_grammar_file = path + 'math_expressions.txt';
+
+
+describe('GrammarParser', function() {
+  var grammar_text;
+  var grammar;
+  
+  beforeEach(function() {
+      this.addMatchers({
+        toEqualProductionRules: function(array) {
+          this.message = function() {
+            return "Expected " + this.actual + " to be array " + array + ".";
+          };
+          var arraysAreSame = function(x, y) {
+            var arraysAreSame = (x.length === y.length);
+            if (arraysAreSame) {
+              for(var i; i < x.length; i++)
+                 if(!x[i].isEqualTo(y[i]))
+                    arraysAreSame = false;
+              return arraysAreSame;
+            }
+            else {
+              return(false);
+            }
+          };
+          return arraysAreSame(this.actual, array);
+        },
+        toEqualProductionRule: function(rule) {
+          this.message = function() {
+            return "Expected " + JSON.stringify(this.actual) + " to be production rule " + JSON.stringify(rule) + ".";
+          };
+          return (this.actual.isEqualTo(rule));
+        }
+      });
+    });
+    
+//S -> (empty)
+  it('should read a text file', function(done) {
+    fs.readFile(minimal_grammar_file, 'utf8', function (error, text) {
+      expect(text).toBeDefined();
+      grammar_text = text;
+      done();
+    });
+  });
+  it('should correctly parse a minial grammar consisting of the rule S -> (empty)', function () {
+    var expected = {};
+    expected.production_rules = [ { 'lhs' : 'S', 'rhs' : [  ], 'head' : 0, 'fs' : null } ];
+    expected.nonterminals = {'S': true};
+    expected.start_symbol = 'S';
+    expected.is_CNF = false;
+    grammar = GrammarParser.parse(grammar_text);
+    expect(grammar.production_rules).toEqualProductionRules(expected.production_rules);
+    expect(grammar.nonterminals).toEqual(expected.nonterminals);
+    expect(grammar.is_CNF).toEqual(expected.is_CNF);
+    expect(grammar.start_symbol).toEqual(expected.start_symbol);
+  });
+
+  it('should recognise nonterminals', function () {
+    expect(grammar.isNonterminal('S')).toEqual(true);
+  });
+
+  it('should recognise terminals', function () {
+    expect(grammar.isTerminal('S')).toEqual(false);
+  });
+  
+  it('should look up rules with left-hand-side S', function () {
+    expect(grammar.rulesWithLHS('S')).toEqualProductionRules([ { lhs : 'S', rhs : [  ], head : 0, fs : null } ]);
+  });
+  
+  it('should look up the start rule', function () {
+    expect(grammar.startRule()).toEqualProductionRule({ lhs : 'S', rhs : [  ], head : 0, fs : null });
+  });
+
+  it('should look up the start symbol', function () {
+    expect(grammar.getStartSymbol()).toEqual('S');
+  });
+  
+
+//S -> NP VP
+//NP -> DT NN
+//PP -> IN NP
+//VP -> VBP NP
+//VP -> work
+  it('should read another text file', function(done) {
+    fs.readFile(grammar_for_CFG_file, 'utf8', function (error, text) {
+      expect(text).toBeDefined();
+      grammar_text = text;
+      done();
+    });
+  });
+  it('should correctly parse a grammar in CNF', function () {
+    var expected = {};
+    expected.production_rules = [ { lhs : 'S', rhs : [ 'NP', 'VP' ], head : 0, fs : null }, 
+                                  { lhs : 'NP', rhs : [ 'DT', 'NN' ], head : 0, fs : null }, 
+                                  { lhs : 'PP', rhs : [ 'IN', 'NP' ], head : 0, fs : null }, 
+                                  { lhs : 'VP', rhs : [ 'VBP', 'NP' ], head : 0, fs : null }, 
+                                  { lhs : 'VP', rhs : [ 'work' ], head : 0, fs : null } ];
+    expected.nonterminals = {'S': true, 'NP': true, 'PP': true, 'VP': true};
+    expected.start_symbol = 'S';
+    expected.is_CNF = true;
+    grammar = GrammarParser.parse(grammar_text);
+    expect(grammar.production_rules).toEqualProductionRules(expected.production_rules);
+    expect(grammar.nonterminals).toEqual(expected.nonterminals);
+    expect(grammar.is_CNF).toEqual(expected.is_CNF);
+    expect(grammar.start_symbol).toEqual(expected.start_symbol);
+  });
+
+  it('should recognise nonterminals', function () {
+    expect(grammar.isNonterminal('S')).toEqual(true);
+    expect(grammar.isNonterminal('NP')).toEqual(true);
+    expect(grammar.isNonterminal('VP')).toEqual(true);
+    expect(grammar.isNonterminal('PP')).toEqual(true);
+    expect(grammar.isNonterminal('DT')).toBeFalsy();
+    expect(grammar.isNonterminal('IN')).toBeFalsy();
+    expect(grammar.isNonterminal('VBP')).toBeFalsy();
+    expect(grammar.isNonterminal('work')).toBeFalsy();
+  });
+
+  it('should recognise terminals', function () {
+    expect(grammar.isTerminal('S')).toEqual(false);
+    expect(grammar.isTerminal('NP')).toEqual(false);
+    expect(grammar.isTerminal('VP')).toEqual(false);
+    expect(grammar.isTerminal('PP')).toEqual(false);
+    expect(grammar.isTerminal('DT')).toEqual(true);
+    expect(grammar.isTerminal('IN')).toEqual(true);
+    expect(grammar.isTerminal('VBP')).toEqual(true);
+    expect(grammar.isTerminal('work')).toEqual(true);
+  });
+  
+  it('should look up rules with left-hand-side S', function () {
+    expect(grammar.rulesWithLHS('S')).toEqualProductionRules([ { lhs : 'S', rhs : [ 'NP', 'VP' ], head : 0, fs : null } ]);
+  });
+
+  it('should look up rules with left-hand-side NP', function () {
+    expect(grammar.rulesWithLHS('NP')).toEqualProductionRules([ { lhs : 'NP', rhs : [ 'DT', 'NN' ], head : 0, fs : null } ]);
+  });
+  
+  it('should look up rules with left-hand-side PP', function () {
+    expect(grammar.rulesWithLHS('PP')).toEqualProductionRules([{'lhs': 'PP', 'rhs': ['IN', 'NP'], 'head': 0, 'fs': null}]);
+  });
+  
+  it('should look up rules with left-hand-side VP', function () {
+    expect(grammar.rulesWithLHS('VP')).toEqualProductionRules([{'lhs': 'VP', 'rhs': ['VBP', 'NP'], 'head': 0, 'fs': null},
+                                                  {'lhs': 'VP', 'rhs': ['work'], 'head': 0, 'fs': null}]);
+  });
+
+  it('should look up the start rule', function () {
+    expect(grammar.startRule()).toEqualProductionRules({'lhs': 'S', 'rhs': ['NP', 'VP'], 'head': 0, 'fs': null});
+  });
+  
+  it('should look up the start symbol', function () {
+    expect(grammar.getStartSymbol()).toEqual('S');
+  });
+
+  it('should look up production rules based on right hand side symbols (for CYK)', function () {
+    expect(grammar.getRulesWithRHS('NP', 'VP')).toEqualProductionRules([{'lhs': 'S', 'rhs': ['NP', 'VP'], 'head': 0, fs: null}]);
+    expect(grammar.getRulesWithRHS('DT', 'NN')).toEqualProductionRules([{'lhs': 'NP', 'rhs': ['DT', 'NN'], 'head': 0, fs: null}]);
+    expect(grammar.getRulesWithRHS('IN', 'NP')).toEqualProductionRules([{'lhs': 'PP', 'rhs': ['IN', 'NP'], 'head': 0, fs: null}]);
+    expect(grammar.getRulesWithRHS('VBP', 'NP')).toEqualProductionRules([{'lhs': 'VP', 'rhs': ['VBP', 'NP'], 'head': 0, fs: null}]);
+    expect(grammar.getRulesWithRHS('DT', 'NN')).toEqualProductionRules([{'lhs': 'NP', 'rhs': ['DT', 'NN'], 'head': 0, fs: null}]);
+  });
+
+//E -> E plus E
+//E -> E minus E
+//E -> E divide E
+//E -> E multiply E
+//E -> number
+  it('should read another text file', function(done) {
+    fs.readFile(math_grammar_file, 'utf8', function (error, text) {
+      expect(text).toBeDefined();
+      grammar_text = text;
+      done();
+    });
+  });
+  
+  it('should correctly parse a context-free grammar (that is not in CNF)', function () {
+    var expected = {};
+    expected.production_rules = [{'lhs': 'E', 'rhs': ['E', 'plus', 'E'], 'head': 1, 'fs': null}, 
+                                 {'lhs': 'E', 'rhs': ['E', 'minus', 'E'], 'head': 1, 'fs': null}, 
+                                 {'lhs': 'E', 'rhs': ['E', 'divide', 'E'], 'head': 1, 'fs': null}, 
+                                 {'lhs': 'E', 'rhs': ['E', 'multiply', 'E'], 'head': 1, 'fs': null}, 
+                                 {'lhs': 'E', 'rhs': ['number'], 'head': 1, 'fs': null}];
+    expected.nonterminals = {'E': true};
+    expected.start_symbol = 'E';
+    expected.is_CNF = false;
+    grammar = GrammarParser.parse(grammar_text);
+    expect(grammar.production_rules).toEqualProductionRules(expected.production_rules);
+    expect(grammar.nonterminals).toEqual(expected.nonterminals);
+    expect(grammar.is_CNF).toEqual(expected.is_CNF);
+    expect(grammar.start_symbol).toEqual(expected.start_symbol);
+   });
+
+  it('should recognise nonterminals', function () {
+    expect(grammar.isNonterminal('E')).toEqual(true);
+  });
+
+  it('should recognise terminals', function () {
+    expect(grammar.isTerminal('E')).toEqual(false);
+  });
+  
+  it('should look up the start rule', function () {
+    expect(grammar.startRule()).toEqualProductionRule({'lhs': 'E', 'rhs': ['E', 'plus', 'E'], 'head': 1, 'fs': null});
+  });
+  
+  it('should look up the start symbol', function () {
+    expect(grammar.getStartSymbol()).toEqual('E');
+  });
+  
+});
