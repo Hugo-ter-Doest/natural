c54507d2b11b5f46525b80623732bd74b0ca6c16
Hugo-ter-Doest <hwl.ter.doest@gmail.com>
2018-11-12 00:05:19 +0100

    Added CYK and Earley parsers for shallow parsing

---
+++ spec/EarleyParser_spec.js
@@ -0,0 +1,164 @@
+/*
+    Unit test for the Earley parser
+    Copyright (C) 2018 Hugo W.L. ter Doest
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+var fs = require('fs');
+
+var Sentence = require('../lib/natural/brill_pos_tagger/lib/Sentence');
+var GrammarParser = require('../lib/natural/parsers/GrammarParser');
+var EarleyParser = require('../lib/natural/parsers/EarleyParser');
+
+var path = './spec/test_data/';
+var math_grammar_file = path + 'math_expressions.txt';
+var CYK_grammar_file = path + 'test_grammar_for_CYK.txt';
+
+
+describe('EarleyParser', function() {
+
+  beforeEach(function() {
+    this.addMatchers({
+      toBeArray: function(array) {
+        this.message = function() {
+          return "Expected " + this.actual + " to be array " + array + ".";
+        };
+        var arraysAreSame = function(x, y) {
+          var arraysAreSame = (x.length === y.length);
+          if (arraysAreSame) {
+            for(var i; i < x.length; i++)
+               if(x[i] !== y[i])
+                  arraysAreSame = false;
+            return arraysAreSame;
+          }
+          else {
+            return(false);
+          }
+        };
+        return arraysAreSame(this.actual, array);
+      }
+    });
+  });
+
+  var grammar_text;
+
+  // E -> E plus E
+  // E -> E minus E
+  // E -> E divide E
+  // E -> E multiply E
+  // E -> number
+  it('should read a text file', function(done) {
+    fs.readFile(math_grammar_file, 'utf8', function (error, text) {
+      expect(text).toBeDefined();
+      grammar_text = text;
+      done();
+    });
+  });
+
+  var grammar;
+  it('should parse the text file with the grammar', function() {
+    grammar = GrammarParser.parse(grammar_text);
+  });
+
+  it('should parse a sentence', function() {
+    //var parser = new ChartParser(grammar);
+    var parser = new EarleyParser(grammar);
+
+    // 2 + 3
+    var taggedSentence = new Sentence();
+    taggedSentence.addTaggedWord('2', 'number');
+    taggedSentence.addTaggedWord('+', 'plus');
+    taggedSentence.addTaggedWord('3', 'number');
+
+    var chart = parser.parse(taggedSentence);
+    var parse_trees = chart.parseTrees(parser.grammar.getStartSymbol(), "earleyitem");
+    expect(parse_trees).toBeArray(['E(E(number(2())),plus(+()),E(number(3())))']);
+
+    // 2 + 3 * 4
+    taggedSentence = new Sentence();
+    taggedSentence.addTaggedWord('2', 'number');
+    taggedSentence.addTaggedWord('+', 'plus');
+    taggedSentence.addTaggedWord('3', 'number');
+    taggedSentence.addTaggedWord('*', 'multiply');
+    taggedSentence.addTaggedWord('4', 'number');
+    chart = parser.parse(taggedSentence);
+    parse_trees = chart.parseTrees(parser.grammar.getStartSymbol(), "earleyitem");
+    parse_trees.sort();
+    expected_parse_trees = [
+      'E(E(E(number(2())),plus(+()),E(number(3()))),multiply(*()),E(number(4())))',
+      'E(E(number(2())),plus(+()),E(E(number(3())),multiply(*()),E(number(4()))))'
+    ].sort();
+    expect(parse_trees).toBeArray(expected_parse_trees);
+  });
+
+
+// Grammar (in ../data/test_grammar_for_CYK.txt)
+//S  -> NP VP
+//NP -> DET N
+//NP -> NP PP
+//PP -> P NP
+//VP -> V NP
+//VP -> VP PP
+
+// Lexicon
+//DET -> the
+//NP -> I
+//N -> man
+//N -> telescope
+//P -> with
+//V -> saw
+//N -> cat
+//N -> dog
+//N -> pig
+//N -> hill
+//N -> park
+//N -> roof
+//P -> from
+//P -> on
+//P -> in
+  it('should read a text file', function(done) {
+    fs.readFile(CYK_grammar_file, 'utf8', function (error, text) {
+      grammar_text = text;
+      done();
+    });
+  });
+
+  it('should parse the text file with the grammar', function() {
+    grammar = GrammarParser.parse(grammar_text);
+  });
+  it('should parse a sentence', function() {
+    parser = new EarleyParser(grammar);
+
+    var taggedSentence = new Sentence();
+    taggedSentence.addTaggedWord('I', 'NP');
+    taggedSentence.addTaggedWord('saw', 'V');
+    taggedSentence.addTaggedWord('the', 'DET');
+    taggedSentence.addTaggedWord('man', 'N');
+    taggedSentence.addTaggedWord('with', 'P');
+    taggedSentence.addTaggedWord('the', 'DET');
+    taggedSentence.addTaggedWord('telescope', 'N');
+
+    chart = parser.parse(taggedSentence);
+    parse_trees = chart.parseTrees(parser.grammar.getStartSymbol(), "earleyitem");
+    parse_trees.sort();
+    expected_parse_trees = [
+      'S(NP(I()),VP(VP(V(saw()),NP(DET(the()),N(man()))),PP(P(with()),NP(DET(the()),N(telescope())))))',
+      'S(NP(I()),VP(V(saw()),NP(NP(DET(the()),N(man())),PP(P(with()),NP(DET(the()),N(telescope()))))))'
+    ].sort();
+    expect(parse_trees).toBeArray(expected_parse_trees);
+  });
+});
+
